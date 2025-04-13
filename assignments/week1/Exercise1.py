a3d7f0da4aeac5ff2af080718447b21c0b4ebbf3
Titus-Git-Hub <titusrichter@icloud.com>
2025-04-13 10:36:41 +0200

    Add files via upload

---
+++ assignments/week1/Exercise1.py
@@ -0,0 +1,28 @@
+#%% md
+# 
+#%% md
+# ### Excercise 1
+# 
+# 
+#%% md
+# Make a little piece of ASCII art with print functions in python. You can first try it out here in the notebook. After you are satisfied with your result, save it as a `.py` file
+# 
+#%% md
+# My ASCII-Artwork:
+#%%
+print("             THE FUTURISTIC SPORTS BATTLE                        ")
+print("                                                                 ")
+print("          ///////                    /////                       ")
+print("         ---------                 ---------                     ")
+print("         | () () |                 | x   x |                     ")
+print("         |   >   |                 |   <   |                     ")
+print("         |  -o-  |                 |  -o-  |                     ")
+print("         ---------                 ---------                     ")
+print("   o=====| NIKE$ |=====o     o=====|ADIDAS€|=====o               ")
+print("         |   -   |                 |   -   |                     ")
+print("         |e-sport|                 |e-sport|                     ")
+print("         ---------                 ---------                     ")
+print("         |       |                 |       |                     ")
+print("         |       |                 |       |                     ")
+print("       <=|       |=>   (=)       <=|       |=>                   ")
+print(" _________________________________________________________       ")
